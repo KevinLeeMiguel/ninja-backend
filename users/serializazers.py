9728798c2ac0f96069ee18a69683b799be24b5de
Kevin Lee Miguel <iradukundaleekevin@gmail.com>
2022-10-27 18:06:17 +0200

    handle file upload

--- users/serializazers.py
+++ users/serializazers.py
@@ -1,11 +1,26 @@
+import imp
 from rest_framework import serializers
 from users.models import User
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 class UserUploadSerializer(serializers.Serializer):
-    file = serializers.FileField()
+    file = serializers.FileField(required=True)
     
-    def validate_file(self, value):
-        return value
+    def validate_file(self, value: InMemoryUploadedFile) -> InMemoryUploadedFile:
+        """_summary_
+
+        Args:
+            value (TemporaryUploadedFile): _description_
+
+        Raises:
+            serializers.ValidationError: _description_
+
+        Returns:
+            TemporaryUploadedFile: _description_
+        """
+        if value.content_type in ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']:
+            return value
+        raise serializers.ValidationError("Please upload an excel file!")
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- users/views.py
+++ users/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import MultiPartParser
+
+from users.serializazers import UserUploadSerializer
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import pandas as pd
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
 class UserView(APIView):
-    
-    parser_classes = [FileUploadParser]
+
+    parser_classes = [MultiPartParser]
+    serializer_class = UserUploadSerializer
 
     def post(self, request):
-       data: dict = request.data['files']
-       
-       if 'users' in data:
-           ...
+        serialized_data = self.serializer_class(data=request.data)
+
+        if serialized_data.is_valid():
+            file: InMemoryUploadedFile = serialized_data.validated_data.get(
+                "file")
+            data = pd.read_excel(file.read())
+            return Response(data=f"uploaded data {len(data)}", status=status.HTTP_201_CREATED)
+        return Response(data=serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
