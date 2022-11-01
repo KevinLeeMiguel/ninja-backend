2a448b4b62b26d38e5eab38089a3633d642f447a
Kevin Lee Miguel <iradukundaleekevin@gmail.com>
2022-11-01 13:55:30 +0200

    comment, clean and refactor

--- users/util.py
+++ users/util.py
@@ -8,16 +8,34 @@ from users.models import DocModel, RedisUserModel, User
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
+
 class UserValidator():
+    """_summary_
+        a validator class for the RedisUserMOdel
+    """
     def __init__(self, redis_user_obj: RedisUserModel) -> None:
+        """_summary_
+            Initiating the validator with a RedisUserModel Instance
+        Args:
+            redis_user_obj (RedisUserModel): _description_
+        """
         self.user = redis_user_obj
         self.error_messages = []
         self.valid = True
 
     def flag_invalid(self):
+        """_summary_
+            Flags the validator instance as invalid
+        """
         self.valid = False
 
-    def validate_national_id(self):
+    def validate_national_id(self) -> bool:
+        """_summary_
+            method to validate the national id
+        Returns:
+            bool: _description_
+        """
+
         pattern = re.compile("[1-9]{1}[0-9]{4}[7,8]{1}[0-9]{10}")
         if re.fullmatch(pattern, self.user.national_id):
             age = datetime.date.today().year - int(self.user.national_id[1:5])
@@ -31,7 +49,12 @@ class UserValidator():
         self.flag_invalid()
         return False
 
-    def validate_names(self):
+    def validate_names(self) -> bool:
+        """_summary_
+            method to validates names
+        Returns:
+            bool: _description_
+        """
         self.user.names = self.user.names.strip()
         if len(self.user.names) > 3:
             return True
@@ -40,7 +63,12 @@ class UserValidator():
         self.flag_invalid()
         return False
 
-    def validate_gender(self):
+    def validate_gender(self) -> bool:
+        """_summary_
+            a method to validate gender
+        Returns:
+            bool: _description_
+        """
         allowed_values = ["Male", "Female", "M", "F"]
         if self.user.gender in allowed_values:
             return True
@@ -49,7 +77,12 @@ class UserValidator():
         self.flag_invalid()
         return False
 
-    def validate_email(self):
+    def validate_email(self) -> bool:
+        """_summary_
+            a method to validate email
+        Returns:
+            bool: _description_
+        """
         pattern = re.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
         self.user.email = self.user.email.strip()
         if re.fullmatch(pattern, self.user.email):
@@ -58,27 +91,43 @@ class UserValidator():
         self.flag_invalid()
         return False
 
-    def validate_phone_number(self):
+    def validate_phone_number(self) -> bool:
+        """_summary_
+            a method to validate phone_number
+        Returns:
+            bool: _description_
+        """
         # see https://regex101.com/r/DsaRfI/1
         pattern = re.compile(
             "/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/gm")
-        self.user.phone_number = self.user.phone_number.strip()
+        self.user.phone_number = f"+{self.user.phone_number.strip()}"
         if re.fullmatch(pattern, self.user.phone_number):
             return True
         self.error_messages.append({"phone_number": "Invalid phone number!"})
         self.flag_invalid()
         return False
 
-    def validate(self):
+    def validate(self) -> bool:
+        """_summary_
+            a method to validate a RedisUserModel instance 
+        Returns:
+            bool: Indicates if the instance is valid
+        """
         self.validate_national_id()
         self.validate_names()
         self.validate_gender()
         self.validate_email()
-        # self.validate_phone_number()
+        self.validate_phone_number()
         return self.valid
 
 
 def validate_and_cache(file: InMemoryUploadedFile, doc_id: str):
+    """_summary_
+        a method to validate and cache to Redis the content of the file
+    Args:
+        file (InMemoryUploadedFile): uploaded file
+        doc_id (str): document id assigned to the file
+    """
     data = pd.read_excel(file.read())
     doc = DocModel(
         doc_id=doc_id,
@@ -119,6 +168,13 @@ def validate_and_cache(file: InMemoryUploadedFile, doc_id: str):
 
 
 def convert_and_save(users: List[RedisUserModel]):
+    """_summary_
+        a method convert RedisUserModels to Django User 
+        instances and commit them to the database.
+    Args:
+        users (List[RedisUserModel]): _description_
+    """
+    
     user_list = []
     for user in users:
         u = User()
--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from symbol import star_expr
 import threading
 from uuid import uuid4
 from rest_framework.viewsets import ViewSet
@@ -7,37 +6,68 @@ from users.models import DocModel, RedisUserModel
 
 from users.serializazers import DocSerializer, RedisUserSerializer, UserUploadSerializer
 from django.core.files.uploadedfile import InMemoryUploadedFile
-import pandas as pd
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
+
 
 from users.util import convert_and_save, validate_and_cache
 
 
 # Create your views here.
 class UserViewSet(ViewSet):
+    """_summary_
+    a set of views to handle file upload, cache, 
+    validaation, progress status, and commit
+    Args:
+        ViewSet (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
 
     parser_classes = [MultiPartParser]
     serializer_class = UserUploadSerializer
     permission_classes = [IsAuthenticated]
 
     def create(self, request) -> Response:
+        """_summary_
+        a view method to handle file upload and cache to Redis 
+        Args:
+            request (_type_): _description_
+
+        Returns:
+            Response: _description_
+        """
         serialized_data = self.serializer_class(data=request.data)
 
         if serialized_data.is_valid():
             file: InMemoryUploadedFile = serialized_data.validated_data.get(
                 "file")
             doc_id = uuid4().hex
+
+            # Initiate a thread that will handle the validation and caching of the uploaded file
             x = threading.Thread(
                 target=validate_and_cache, args=(file, doc_id))
+            # start the validation and caching thread
             x.start()
             return Response(data={"doc_id": doc_id}, status=status.HTTP_201_CREATED)
         return Response(data=serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=True, methods=['get'])
     def get_doc_data(self, request, pk) -> Response:
+        """_summary_
+            a method to retrieve the uploaded file status 
+            and the uploaded data with validation errors if any
+        Args:
+            request (_type_): django request object
+            pk (_type_): the document id returned after the upload was successful
+
+        Returns:
+            Response: _description_
+        """
         users = RedisUserModel.find(RedisUserModel.doc_id == pk).all()
         doc = DocModel.find(DocModel.doc_id == pk).all()
         if len(doc) > 0:
@@ -49,16 +79,26 @@ class UserViewSet(ViewSet):
         return Response(data="Doc Not found!", status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=True, methods=['get'])
+    @transaction.atomic
     def commit(self, request, pk) -> Response:
+        """_summary_
+            a method to commit valid entries from the uploaded file
+        Args:
+            request (_type_): django request object
+            pk (_type_): the document id returned after the upload was successful
+
+        Returns:
+            Response: _description_
+        """
         doc = DocModel.find(DocModel.doc_id == pk).all()
         if len(doc) > 0:
             if doc[0].status != "Completed":
                 error_message = "Doc validation still in progress" if doc[0].status == "InProgress" else "Doc validation failed! try uploading a new file"
                 return Response(data=error_message, status=status.HTTP_400_BAD_REQUEST)
             users = RedisUserModel.find((RedisUserModel.doc_id == pk) & (RedisUserModel.valid == "True")).all()
+            # determine the number of batches we need in order to bulksave 2000 entries at a time
             j = int(len(users)/2000)
             batches = j if j%2000 == 0 else j+1
-            print(len(users))
             for i in range(0,batches):
                 if i == batches-1:
                     start = 2000*i
